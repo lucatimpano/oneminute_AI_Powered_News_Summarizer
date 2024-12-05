6cfe6b127c1e74bc074aaff7a5f3167e9c01ad6f
Luca Timpano <lucatimpano19@gmail.com>
2024-12-05 17:55:39 +0100

    Retrofit setup

---
+++ app/src/main/java/com/example/oneminute/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.oneminute.util
+
+class Constants {
+    //contiene un aporzione di codice,  se richiamato con le annotazioni scrivera il codice in automatico
+    companion object{
+        const val API_KEY = "6c3041a1fa834db9808864e30bef830c"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        //Numerto di notizie che visualizzo in una singola pagina
+        const val QUERY_PAGE_SIZE = 20
+
+
+    }
+}
